495e265b8d93541e74eaf2c8fc7fe1d9624b4415
Alex Silverstein <aslvrstn@gmail.com>
2013-05-03 02:03:57 -0400

    Find imports

--- gitmonkey.js
+++ gitmonkey.js
@@ -1,3 +1,27 @@
+function githubMain() {
+  var lines = document.getElementsByClassName('line');
+  // There should only be one line beginning with 'package'
+  packagedef = tokenizeGithubPath(linesStartingWith(lines, 'package')[0]);
+  console.log(linesStartingWith(lines, 'import'));
+}
+
+function googleCodeMain() {
+  var lines = document.getElementsByClassName('source');
+  // There should only be one line beginning with 'package'
+  packagedef = tokenizeGoogleCodePath(linesStartingWith(lines, 'package')[0]);
+  console.log(linesStartingWith(lines, 'import'));
+}
+
+function tokenizeGithubPath(line) {
+  var toks = Array.prototype.slice.call(line.children, 1, -1);
+  return removePeriodsAndTrim(toks);
+}
+
+function tokenizeGoogleCodePath(line) {
+  var toks = Array.prototype.slice.call(line.children, 1, -2);
+  return removePeriodsAndTrim(toks);
+}
+
 function linesStartingWith(lines, word) {
   var ret = [];
   for (i=0; i<lines.length; i++) {
@@ -7,26 +31,6 @@ function linesStartingWith(lines, word) {
     }
   }
   return ret;
-}
-
-function getGithubPackageDef() {
-  var lines = document.getElementsByClassName('line');
-  return tokenizeGithubPath(linesStartingWith(lines, 'package')[0]);
-}
-
-function tokenizeGithubPath(line) {
-  var toks = Array.prototype.slice.call(line.children, 1, -1);
-  return removePeriodsAndTrim(toks);
-}
-
-function getGoogleCodePackageDef() {
-  var lines = document.getElementsByClassName('source');
-  return tokenizeGoogleCodePath(linesStartingWith(lines, 'package')[0]);
-}
-
-function tokenizeGoogleCodePath(line) {
-  var toks = Array.prototype.slice.call(line.children, 1, -2);
-  return removePeriodsAndTrim(toks);
 }
 
 function removePeriodsAndTrim(tokens) {
