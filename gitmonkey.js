64d1ab1dc1a919e5bef3a675b795011cac8dafb1
Alex Silverstein <aslvrstn@gmail.com>
2013-05-03 23:48:03 -0400

    Working on both github and google code. Fails on static imports

--- gitmonkey.js
+++ gitmonkey.js
@@ -7,9 +7,18 @@ function githubMain() {
   // Just assume everything is under "com.example" for now
   var rootPackage = packagedef.slice(0, 2);
 
+  var currentFile = new RegExp(rootPackage.join('/') + ".*$");
+  var baseUrl = document.URL.replace(currentFile, "");
+
   for (i=0; i<imports.length; i++) {
-    var imp = imports[i].children[1].innerHTML.split('.');
-    if (JSON.stringify(imp.slice(0, 2)) == JSON.stringify(rootPackage)) {
+    var line = imports[i];
+    var imp = line.children[line.length-2]; // Final child is semicolon
+    var impstr = imp.innerHTML.split('.');
+
+    // If this file is under the package subset within this repo...
+    if (JSON.stringify(impstr.slice(0, 2)) == JSON.stringify(rootPackage)) {
+      var linkUrl = baseUrl + impstr.join('/') + '.java';
+      imp.innerHTML = '<a href=' + linkUrl + '>' + imp.innerHTML + '</a';
     }
   }
 }
@@ -18,7 +27,28 @@ function googleCodeMain() {
   var lines = document.getElementsByClassName('source');
   // There should only be one line beginning with 'package'
   packagedef = tokenizeGoogleCodePath(linesStartingWith(lines, 'package')[0]);
-  console.log(linesStartingWith(lines, 'import'));
+  var imports = linesStartingWith(lines, 'import');
+
+  // Just assume everything is under "com.example" for now
+  var rootPackage = packagedef.slice(0, 2);
+
+  var currentFile = new RegExp(rootPackage.join('/') + ".*$");
+  var baseUrl = document.URL.replace(currentFile, "");
+
+  for (i=0; i<imports.length; i++) {
+    var line = imports[i];
+    var impstr = tokenizeGoogleCodePath(line);
+
+    // If this file is under the package subset within this repo...
+    if (JSON.stringify(impstr.slice(0, 2)) == JSON.stringify(rootPackage)) {
+      var linkUrl = baseUrl + impstr.join('/') + '.java';
+      var lchildren = line.children;
+      // First element is 'import', final two are semicolon and trailing junk
+      for (c=1; c<line.children.length-2; c++) {
+        lchildren[c].innerHTML = '<a href=' + linkUrl + '>' + lchildren[c].innerHTML + '</a';
+      }
+    }
+  }
 }
 
 function tokenizeGithubPath(line) {
